c2b84c91b81280355b0c5eecccf4dc0d0d1e31a2
SebaAguiar <sebaaguiar08@gmail.com>
2023-10-22 18:06:57 -0300

    new table

--- src/app/ventas/page.jsx
+++ src/app/ventas/page.jsx
@@ -8,39 +8,63 @@ const Ventas = () => {
   const [fromDate, setFromDate] = useState('')
   const [toDate, setToDate] = useState('')
   const [ventas, setVentas] = useState([])
+  const [ventasCopy, setVentasCopy] = useState([])
   const [totalGranancia, setTotalGanancia] = useState(0)
   const [totalClassicBunuelos, setTotalClassicBunuelos] = useState(0)
   const [totalSpecialBunuelo, setTotalSpecialBunuelo] = useState(0)
 
   useEffect(() => {
     return async () => {
-      if(!ventas.length) {
+      if(!ventas.length && !ventasCopy.length) {
         const dbVentas = await axios.get('/api/ventas')
         setVentas(dbVentas.data.response)
+        setVentasCopy(dbVentas.data.response)
       }
     }
-  }, [ventas])
+  }, [ventas, ventasCopy])
 
-  console.log(ventas)
-  // useEffect(() => {
-  //   if(!totalGranancia) {
-  //     const ganancia = 0
-  //     for (var i = 0; i < ventas.length; i++) {
-  //       ganancia += ventas[i].money;
-  //     }
-  //     setTotalGanancia(ganancia)
-  //   }
-  //   if(!totalClassicBunuelos) {
-  //     const classic = 0
-  //     for (var i = 0; i < ventas.length; i++) {
-  //       sumaTotal += arregloObjetos[i].costo;
-  //     }
-  //   } 
-  // })
+  useEffect(() => {
+    if(!totalGranancia) {
+      let ganancia = 0
+      for (let i = 0; i < ventas.length; i++) {
+        ganancia += ventas[i].money;
+      }
+      setTotalGanancia(ganancia)
+    }
+    if(!totalClassicBunuelos) {
+      let classic = 0
+      for (let i = 0; i < ventas.length; i++) {
+        classic += ventas[i].classic;
+      }
+      setTotalClassicBunuelos(classic)
+    }
+    if(!totalSpecialBunuelo) {
+      let special = 0
+      for(let i = 0; i < ventas.length; i++) {
+        special += ventas[i].special
+      }
+      setTotalSpecialBunuelo(special)
+    }
+  }, [totalGranancia, totalClassicBunuelos, totalSpecialBunuelo, ventas])
   
   const handleToDateChange = (e) => {
     setToDate(e.target.value)
     console.log(toDate)
+  }
+
+  const handleToDayChange = (e) => {
+    if(!today) {
+      setVentas(ventasCopy)
+    } else {
+      let fechaHoy = new Date()
+      let filtered = ventas.filter((bun) => {
+        return (    
+          bun.createdAt.getDate() === fechaHoy.getDate() &&
+          bun.createdAt.getMonth() === fechaHoy.getMonth() &&
+          bun.createdAt.getFullYear() === fechaHoy.getFullYear())
+      })
+      setVentas(filtered)
+    }
   }
 
   return (
@@ -48,7 +72,7 @@ const Ventas = () => {
       <h1>Ventas</h1>
       <div className='flex'>
         <div className='flex flex-col-reverse items-center'>
-          <input type="checkbox" />
+          <input onChange={handleToDayChange} value={today} type="checkbox" />
           <label>Hoy</label>
         </div>
         <div className='flex flex-col-reverse items-center'>
@@ -73,9 +97,9 @@ const Ventas = () => {
         </thead>
         <tbody>
           <tr>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td className='whitespace-nowrap py-4 px-6'>{totalClassicBunuelos}</td>
+            <td className='whitespace-nowrap py-4 px-6'>{totalSpecialBunuelo}</td>
+            <td className='whitespace-nowrap py-4 px-6'>{totalGranancia}</td>
           </tr>
         </tbody>
       </table>
